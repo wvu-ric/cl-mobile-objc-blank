44b015eb725169db2d54d69f570e6195deb3bb19
stadelmanma <stadelmanma@gmail.com>
2016-02-11 16:39:42 -0500

    Assignment Completed

---
+++ Blank/Blank.h
@@ -0,0 +1,14 @@
+//
+//  Blank.h
+//  Blank
+//
+//  Created by Matthew Stadelman on 2/11/16.
+//  Copyright (c) 2016 CodeLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blank : NSObject
+
+
+@end
