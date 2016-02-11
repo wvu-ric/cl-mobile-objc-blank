8bda5a33de9a5364bb64f084767ecf6e1e12eec3
mckinneyra <ramckinney@mix.wvu.edu>
2016-02-11 12:25:17 -0500

    Assignment Complete

---
+++ Blank/Blank/Blank.m
@@ -0,0 +1,21 @@
+//
+//  Blank.m
+//  Blank
+//
+//  Created by Rachael Alees McKinney on 2/11/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import "Blank.h"
+
+@implementation Blank
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _nothing = 0;
+    }
+    return self;
+}
+@end
