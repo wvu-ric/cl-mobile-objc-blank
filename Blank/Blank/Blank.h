71fd83a9097b63328c0af24dfa5bae67e5ad4817
jmathen2 <jmathen2@mix.wvu.edu>
2016-02-18 00:33:58 -0500

    "Blank" Assignment
    When making the new “Blank” subclass I made a method and three
    properties in the interface and implementation files for creating the
    class and then used the methods and properties (I made properties just
    to make sure I knew how they worked) to display NSLog messages in the
    main m file that runs the code…Please let me know if that is what you
    had in mind for this assignment

---
+++ Blank/Blank/Blank.h
@@ -0,0 +1,20 @@
+//
+//  Blank.h
+//  Blank
+//
+//  Created by Josh Matheny on 2/17/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blank : NSObject
+
+@property NSString *someThing;
+@property NSDate *someDate;
+@property int someNumber;
+
+- (void) myBlankObject;
+
+
+@end
---
+++ Blank/Blank/Blank.m
@@ -0,0 +1,17 @@
+//
+//  Blank.m
+//  Blank
+//
+//  Created by Josh Matheny on 2/17/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import "Blank.h"
+
+@implementation Blank
+
+- (void) myBlankObject {
+    NSLog(@"My blank method works...");
+}
+
+@end
